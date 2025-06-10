6623388ee14d578429afed687f9236f9e75edb11
FreitasPriscila <priscilafreitasmartins0@gmail.com>
2025-06-09 21:30:47 -0300

    oi

--- src/pages/usuario.jsx
+++ src/pages/usuario.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import styles from '../styles/usuario.module.css';
 import Coruja from "../assets/img/coruja-mascote.png";
 import { useNavigate } from 'react-router-dom';
-
+//testando git
 // Ícone de fechar
 const CloseIcon = () => (
   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
